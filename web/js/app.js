0542700800aa36b365873c35488975acc5bb8e2f
Hiroki Inagaki <hiroki@inagaki.in>
2014-02-23 08:14:41 +0900

    Change client framework from Backbone.js to AngularJS

--- web/js/app.js
+++ web/js/app.js
@@ -1,4 +1,6 @@
-(function(){
+angular.module("accountBook", ["ngResource"]);
+
+function input($scope, $http){
 
 	$.datepicker.setDefaults( $.datepicker.regional[ "ja" ] );
 
@@ -24,259 +26,179 @@
 		numeral.defaultFormat('$0,0');
 
 		$.ajax({
-  			url: "/api/account_service/variable_expenses/" + month.replace("/", "-") + "/total",
-  			success: function(html){
+			url: "/api/account_service/variable_expenses/" + month.replace("/", "-") + "/total",
+			success: function(html){
 				varExTotal = parseInt(html);
-  			}
+			}
 		}).pipe(function() {
 			return $.ajax({
-  				url: "/api/account_service/fixed_expenses/" + month.replace("/", "-") + "/total",
-  				success: function(html){
+				url: "/api/account_service/fixed_expenses/" + month.replace("/", "-") + "/total",
+				success: function(html){
 					fixExTotal = parseInt(html);
-  				}
+				}
 			});
 		}).done(function() {
 			return $.ajax({
-	  			url: "/api/account_service/revenues/" + month.replace("/", "-") + "/total",
-	  			success: function(html){
-				revenueTotal = parseInt(html);
-				subtotal = revenueTotal - (varExTotal + fixExTotal);
-				$("#varExpenseTotal").text(numeral(varExTotal).format());
-				$("#fixExpenseTotal").text(numeral(fixExTotal).format());
-				$("#revenueTotal").text(numeral(revenueTotal).format());
-				$("#subtotal").text(numeral(subtotal).format());
-	  			}
+				url: "/api/account_service/revenues/" + month.replace("/", "-") + "/total",
+				success: function(html){
+					revenueTotal = parseInt(html);
+					subtotal = revenueTotal - (varExTotal + fixExTotal);
+					$("#varExpenseTotal").text(numeral(varExTotal).format());
+					$("#fixExpenseTotal").text(numeral(fixExTotal).format());
+					$("#revenueTotal").text(numeral(revenueTotal).format());
+					$("#subtotal").text(numeral(subtotal).format());
+				}
 			});
 		})
 	}
 
-	var observer = {
-		countSync:function () {
-			console.log("+++observer.showArguments: ");
-        _.each(arguments, function (item, index) {
-            console.log("  +++arguments[" + index + "]: " + JSON.stringify(item));
-        });
-			syncCount++;
-			console.log("sync count: " + syncCount);
-		}
-	};
-	_.extend(observer, Backbone.Events);
-
-	var VarExpense = Backbone.Model.extend({
-		urlRoot: "/api/account_service/variable_expenses",
-		idAttribute: "_id",
-		defaults: {
-			"date": date,
-			"category": "",
-			"item": "",
-			"price": 0
-		}
-	});
-
-	var VarExpenseList = Backbone.Collection.extend({
-		model: VarExpense,
-		url: "/api/account_service/variable_expenses",
-		dataType: 'jsonp',
-		save: function(){
-			Backbone.sync("update", this, {
-				success: function(){
-					console.log("save successfully");
-					updateTable("submitVarExpense");
-					showTotals();
-				},
-				error: function(model, res){
-					console.log("error saving :" + res);
-				}
-
-			});
-		}
-	});
-
-	var FixExpense = Backbone.Model.extend({
-		urlRoot: "/api/account_service/fixed_expenses",
-		idAttribute: "_id",
-		defaults: {
-			"month": "",
-			"item": "",
-			"price": 0
-		}
-	});
-
-	var FixExpenseList = Backbone.Collection.extend({
-		model: FixExpense,
-		url: "/api/account_service/fixed_expenses",
-		save: function(){
-			Backbone.sync("update", this, {
-				success: function(){
-					console.log("save successfully");
-					updateTable("submitFixExpense");
-					showTotals();
-				},
-				error: function(){
-					console.log("error saving");
-				}
-
-			});
-		}
-	});
-
-	var Revenue = Backbone.Model.extend({
-		urlRoot: "/api/account_service/revenues",
-		idAttribute: "_id",
-		defaults: {
-			"month": "",
-			"item": "",
-			"price": 0
-		}
-	});
-
-	var RevenueList = Backbone.Collection.extend({
-		model: Revenue,
-		url: "/api/account_service/revenues",
-		save: function(){
-			Backbone.sync("update", this, {
-				success: function(){
-					console.log("save successfully");
-					updateTable("submitRevenue");
-					showTotals();
-				},
-				error: function(){
-					console.log("error saving");
-				}
-
-			});
-		}
-	});
-
-	var VarExCategory = Backbone.Model.extend({
-		urlRoot: "/api/account_service/variable_expense_categories",
-		idAttribute: "_id",
-	});
-
-	var VarExCategoryList = Backbone.Collection.extend({
-		model: VarExCategory,
-		url: "/api/account_service/variable_expense_categories"
-	});
-
-	var varExCategories = new VarExCategoryList();
-	var varExpenses = new VarExpenseList();
+	var varExCategories;
+	var varExpenses;
 	var varExTable;
 	var $varExContainer = $("#varExpense");
 	var varExHandsontable;
 
-	var fixExpenses = new FixExpenseList();
+	var fixExpenses;
 	var fixExTable;
 	var $fixExContainer = $("#fixExpense");
 	var fixExHandsontable;
 
-	var revenues = new RevenueList();
+	var revenues;
 	var revenueTable;
 	var $revenueContainer = $("#revenue");
 	var revenueHandsontable;
 
-	varExCategories.fetch({
-		success: function() {
-			console.log("Fetch varExCategories: " + JSON.stringify(varExCategories));
-		}
-	}).pipe(function() {
-		return varExpenses.fetch({
-			success: function() {
-				console.log("Fecth varExList: " + JSON.stringify(varExpenses));
-				varExTable = JSON.parse(JSON.stringify(varExpenses.where({date: date})));
-				$varExContainer.handsontable({
-					data : varExTable,
-					//colWidths: 120,
-					rowHeaders: true,
-					colHeaders: true,
-					contextMenu: ['row_above', 'row_below', 'hsep1', 'undo', 'redo'],
-					minSpareRows: 1,
-					fillHandle: true,
-					colHeaders: ["カテゴリ", "項目名", "金額"],
-					columns: [
+	promiseA = $http({
+		method : 'GET',
+		url : "/api/account_service/variable_expense_categories",
+	}).
+		then(function(data, status, headers, config) {
+		varExCategories = data.data;
+	});
+	promiseB = promiseA.then(function() {
+		$http({
+			method : 'GET',
+			url : "/api/account_service/variable_expenses",
+		}).
+			then(function(data, status, headers, config) {
+			varExTable = JSON.parse(JSON.stringify(data.data.filter(function(item, index){
+				if (item.date == date) return true;
+			})));
+			$varExContainer.handsontable({
+				data : varExTable,
+				//colWidths: 120,
+				rowHeaders: true,
+				colHeaders: true,
+				contextMenu: ['row_above', 'row_below', 'hsep1', 'undo', 'redo'],
+				minSpareRows: 1,
+				fillHandle: true,
+				colHeaders: ["カテゴリ", "項目名", "金額"],
+				columns: [
 					{
-						data: "category",
-						type: 'dropdown',
-						source: varExCategories.pluck("category")
-					},
+					data: "category",
+					type: 'dropdown',
+					source: pluck(varExCategories, "category")
+				},
+				{data: "item"},
+				{data: "price",
+					type: 'numeric',
+					format: '$0,0',
+					language: 'ja'}
+				]
+			});
+			varExHandsontable = $varExContainer.handsontable('getInstance');
+		})
+	});
+	promiseC = promiseB.then(function() {
+		$http({
+			method : 'GET',
+			url : "/api/account_service/fixed_expenses",
+		}).
+			then(function(data, status, headers, config) {
+			fixExTable = JSON.parse(JSON.stringify(data.data.filter(function(item, index){
+				if (item.month == month) return true;
+			})));
+			$fixExContainer.handsontable({
+				data : fixExTable,
+				//colWidths: 120,
+				rowHeaders: true,
+				colHeaders: true,
+				contextMenu: ['row_above', 'row_below', 'hsep1', 'undo', 'redo'],
+				minSpareRows: 1,
+				fillHandle: true,
+				colHeaders: ["項目名", "金額"],
+				columns: [
 					{data: "item"},
 					{data: "price",
 						type: 'numeric',
 						format: '$0,0',
 						language: 'ja'}
-					]
-				});
-				varExHandsontable = $varExContainer.handsontable('getInstance');
-			}
-		});
-	}).pipe(function() {
-		return fixExpenses.fetch({
-			success: function() {
-				console.log("Fecth fixExList: " + JSON.stringify(fixExpenses));
-				fixExTable = JSON.parse(JSON.stringify(fixExpenses.where({month: month})));
-				$fixExContainer.handsontable({
-					data : fixExTable,
-					//colWidths: 120,
-					rowHeaders: true,
-					colHeaders: true,
-					contextMenu: ['row_above', 'row_below', 'hsep1', 'undo', 'redo'],
-					minSpareRows: 1,
-					fillHandle: true,
-					colHeaders: ["項目名", "金額"],
-					columns: [
+				]
+			});
+			fixExHandsontable = $fixExContainer.handsontable('getInstance');
+		})
+	});
+	promiseD = promiseC.then(function() {
+		$http({
+			method : 'GET',
+			url : "/api/account_service/revenues",
+		}).
+			then(function(data, status, headers, config) {
+			revenueTable = JSON.parse(JSON.stringify(data.data.filter(function(item, index){
+				if (item.month == month) return true;
+			})));
+			$revenueContainer.handsontable({
+				data : revenueTable,
+				//colWidths: 120,
+				rowHeaders: true,
+				colHeaders: true,
+				contextMenu: ['row_above', 'row_below', 'hsep1', 'undo', 'redo'],
+				minSpareRows: 1,
+				fillHandle: true,
+				colHeaders: ["項目名", "金額"],
+				columns: [
 					{data: "item"},
 					{data: "price",
 						type: 'numeric',
 						format: '$0,0',
 						language: 'ja'}
-					]
-				});
-				fixExHandsontable = $fixExContainer.handsontable('getInstance');
-			}
-		});
-	}).done(function() {
-		return revenues.fetch({
-			success: function() {
-				console.log("Fecth revenueList: " + JSON.stringify(revenues));
-				revenueTable = JSON.parse(JSON.stringify(revenues.where({month: month})));
-				$revenueContainer.handsontable({
-					data : revenueTable,
-					//colWidths: 120,
-					rowHeaders: true,
-					colHeaders: true,
-					contextMenu: ['row_above', 'row_below', 'hsep1', 'undo', 'redo'],
-					minSpareRows: 1,
-					fillHandle: true,
-					colHeaders: ["項目名", "金額"],
-					columns: [
-					{data: "item"},
-					{data: "price",
-						type: 'numeric',
-						format: '$0,0',
-						language: 'ja'}
-					]
-				});
-				revenueHandsontable = $revenueContainer.handsontable('getInstance');
-			}
-		});
+				]
+			});
+			revenueHandsontable = $revenueContainer.handsontable('getInstance');
+		})
 	});
 
-	function updateTable(element) {
-		var updateList = getUpdateList(element);
-		updateList.fetch({
-			success: function() {
-				console.log("Fecth varExCategoryList: " + JSON.stringify(updateList));
-				var table = JSON.parse(JSON.stringify(updateList.where(getWhere(element))));
-				getHandsontable(element).loadData(table);
-			}
-		});
+	function pluck(array, property) {
+		var rv = [];
+
+		for (i = 0; i < array.length; ++i) {
+			rv[i] = array[i][property];
+		}
+
+		return rv;
 	}
+
+	function updateTable(element) {
+		var url = getUpdateList(element);
+		$http({
+			method : 'GET',
+			url : url,
+		}).
+			success(function(data, status, headers, config) {
+			console.log("Fecth varExCategoryList: " + JSON.stringify(data));
+			var table = JSON.parse(JSON.stringify(data.filter(function(item, index){
+				return getWhere(element, item);
+			})));
+			getHandsontable(element).loadData(table);
+		});
+	};
 
 	$("#date").change(function () {
 		console.log("date changed");
 		date = $("#date").val();
 		updateTable("submitVarExpense");
 	});
-	
+
 	function getCols(element) {
 		switch (element) {
 			case "submitVarExpense":
@@ -290,27 +212,14 @@
 		}
 	}
 
-	function getTarget(element) {
-		switch (element) {
-			case "submitVarExpense":
-				return new VarExpense();
-			case "submitFixExpense":
-				return new FixExpense();
-			case "submitRevenue":
-				return new Revenue();
-			default:
-				return null;
-		}
-	}
-
 	function getTargetList(element) {
 		switch (element) {
 			case "submitVarExpense":
-				return new VarExpenseList();
+				return "/api/account_service/variable_expenses";
 			case "submitFixExpense":
-				return new FixExpenseList();
+				return "/api/account_service/fixed_expenses";
 			case "submitRevenue":
-				return new RevenueList();
+				return "/api/account_service/revenues";
 			default:
 				return null;
 		}
@@ -332,11 +241,11 @@
 	function getUpdateList(element) {
 		switch (element) {
 			case "submitVarExpense":
-				return varExpenses;
+				return "/api/account_service/variable_expenses";
 			case "submitFixExpense":
-				return fixExpenses;
+				return "/api/account_service/fixed_expenses";
 			case "submitRevenue":
-				return revenues;
+				return "/api/account_service/revenues";
 			default:
 				return null;
 		}
@@ -355,16 +264,16 @@
 		}
 	}
 
-	function getWhere(element) {
+	function getWhere(element, item) {
 		switch (element) {
 			case "submitVarExpense":
-				return {date: date};
+				return (item.date == date);
 			case "submitFixExpense":
-				return {month: month};
+				return (item.month == month);
 			case "submitRevenue":
-				return {month: month};
+				return (item.month == month);
 			default:
-				return null;
+				return false;
 		}
 	}
 
@@ -372,7 +281,8 @@
 		var id = $(this).attr("id");
 		var previous = {"category": null, "item": null, "price": null};
 		var cols = getCols(id);
-		var targets = getTargetList(id);
+		var targets = new Array();
+		var url	= getTargetList(id);
 		var table = getTable(id);
 		var rows = table.length;
 		var breakFlag = false;
@@ -415,9 +325,18 @@
 				if (table[row]["_id"] == null) {
 					table[row]["_id"] = undefined;
 				}
-				targets.add(table[row]);
+				targets.push(table[row]);
 			}
-			targets.save(null, {});
+			$http({
+				method : 'PUT',
+				url : url,
+				data : targets,
+			}).
+			then(function(data, status, headers, config) {
+				varExCategories = data.data;
+				updateTable("submitVarExpense");
+				showTotals();
+			});
 		}
 	});
 
@@ -429,7 +348,7 @@
 		console.log("month clicked");
 		$(".list-group-item").attr("class", "list-group-item");
 		$(this).attr("class", "list-group-item active");
-	  	$("#month").text($(this).attr("id"));
+		$("#month").text($(this).attr("id"));
 		$("#date").val($("#month").text() + "/01");
 		date = $("#date").val();
 		month = $("#month").text();
@@ -440,4 +359,4 @@
 		return false;
 	});
 
-}());
+};
